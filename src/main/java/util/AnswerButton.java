83a634c1a5d38943cd35668e225ebb2f97e49810
Manwe <mathieu.jourdan.zola@gmail.com>
2023-11-09 20:41:38 +0100

    Add border on the clicked button to show a feedback and a reminder for the player in game

--- src/main/java/util/AnswerButton.java
+++ src/main/java/util/AnswerButton.java
@@ -1,13 +1,21 @@
 package util;
 
 import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import view.MenuScene;
 
 import java.util.List;
 
-public class AnswerButton extends Button {
+public class AnswerButton extends Button
+{
+
+    private final BorderWidths borderWidths = new BorderWidths(2.0);
+    private final CornerRadii cornerRadii = new CornerRadii(5.0);
+    private final Border border = new Border(new BorderStroke(Color.BLACK,
+            BorderStrokeStyle.SOLID, cornerRadii, borderWidths));
     private boolean isClicked;
     public AnswerButton(boolean buttonIsClicked, String text)
     {
@@ -21,6 +29,7 @@ public class AnswerButton extends Button {
         answerButton.setOnAction(event -> {
         answerButton.checkOthersButtonClicked(buttonList);
         answerButton.setClicked(true);
+        answerButton.setBorder(border);
         validateButton.setDisable(false);
         });
     }
@@ -28,6 +37,7 @@ public class AnswerButton extends Button {
     public void checkOthersButtonClicked(List<AnswerButton> buttonList)
     {
         for (AnswerButton answerButton : buttonList) {
+            answerButton.setBorder(null);
             if (answerButton.isClicked) {
                 answerButton.isClicked = false;
             }
